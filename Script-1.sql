0de949c8c0ce25468dd5c58e05d6784a73d81b84
ChrisMg1 <christoph.maget@outlook.com>
2021-11-09 16:55:29 +0100

    update

--- Script-1.sql
+++ Script-1.sql
@@ -1,3 +1,34 @@
+
+-- create and specify table
+create table LVM_OD (
+FROMZONE_NO int,
+FROMZONE_NAME text,
+FROMZONE_XCOORD float,
+FROMZONE_YCOORD float,
+TOZONE_NO int,
+TOZONE_NAME text,
+TOZONE_XCOORD float,
+TOZONE_YCOORD float,
+DIRECTDIST float,
+Time_PrT float,
+Time_PuT float,
+Demand_Pkw float,
+Demand_PkwM float,
+Demand_PuT float,
+Demand_all float,
+beeline_speed float,
+speed_ratio float,
+wkt_geom text
+)
+
+--- import csv
+COPY lvm_od
+FROM 'C:\temp\test.csv' 
+DELIMITER E'\t' 
+CSV HEADER;
+
+
+
 create table GPS_full (
 assetId text,
 country text,
