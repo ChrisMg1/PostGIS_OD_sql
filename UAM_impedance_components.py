bf6c5b0b89cb86ae50bbc9227e1782b51b0fe84e
ChrisMg1 <christoph.maget@outlook.com>
2022-07-12 13:58:48 +0200

    changes at work

--- UAM_impedance_components.py
+++ UAM_impedance_components.py
@@ -21,6 +21,8 @@ def bathtub2 (x_in, l, r, a1):
 def imp_rel2(x_in, p, a2):
     return (1 / (1 + np.exp(a2 * (x_in - p)) ))
 
+def PAX_max(x_in, d, w, s):
+	return 1-d*maxwell.pdf(w*(x_in - s))
 
 ## Create values for variables
 max_PAX = 15
@@ -66,23 +68,6 @@ plt.show()
 plt.clf()
 
 
-
-## plot capacity impedence (Maxwell)
-plt.figure()
-plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
-
-## play with parameters and plot
-plt.plot(x_PAX, 1-1.7*maxwell.pdf(0.35*x_PAX))
-
-
-plt.xlabel('Demand [PAX / flight]')
-plt.ylabel('UAM Impedance (normalized)')
-
-plt.savefig('plots/demand_Maxwell.png')
-plt.show()
-plt.clf()
-
-
 ## plot distance impedence (bathtub)
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
@@ -100,6 +85,27 @@ plt.savefig('plots/Distance_bathtub.png')
 plt.show()
 plt.clf()
 
-test_PAX = 60
+
+## plot capacity impedence (Maxwell)
+plt.figure()
+plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
+
+
+PAX_vals = []
+for i in x_PAX:
+    PAX_vals.append(PAX_max(i, 1.7034, 0.35, 0))
+## play with parameters and plot
+plt.plot(x_PAX, PAX_vals)
+
+#print(min(PAX_vals))
+
+
+plt.xlabel('Demand [PAX / flight]')
+plt.ylabel('UAM Impedance (normalized)')
+
+plt.savefig('plots/demand_Maxwell.png')
+plt.show()
+plt.clf()
+
 
 # todo: im SQL anpassen
