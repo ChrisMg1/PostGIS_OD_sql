57f8d4a9f3951eac86627de37f3bfb3d35456357
ChrisMg1 <christoph.maget@outlook.com>
2024-04-11 23:45:22 +0200

    240411 ausgemistet

--- Script-5.sql
+++ Script-5.sql
@@ -91,4 +91,9 @@ select * from my_points;
 SELECT COUNT (DISTINCT geom) FROM my_points;
 
 
+ SELECT DISTINCT ON (dauz.bast_id) dauz.bast_id, strassen_polyline2.abs, ST_Distance(ST_SetSRID(ST_Makepoint(p.x, p.y),4326), dauz.geom)
+FROM points dauz
+    LEFT JOIN roads r ON ST_DWithin(ST_SetSRID(ST_Makepoint(p.x, p.y),4326), r.geom, 30.48)
+ORDER BY p.id, ST_Distance(ST_SetSRID(ST_Makepoint(p.x, p.y),4326), r.geom);
+
 
--- Script-7.sql
+++
@@ -1,7 +0,0 @@
--- add spatial column
-
-ALTER TABLE dauz ADD COLUMN geom_tangent geometry(multilinestring,4326);
-
--- add geometries and coordinate system
-
-update dauz set geom = st_setsrid(st_makepoint(longitude, latitude), 4326)
