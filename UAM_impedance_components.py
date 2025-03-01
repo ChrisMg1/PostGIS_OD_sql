f60218c7533bc39e00fa1bf9daa851c3103ac4cc
ChrisMg1 <christoph.maget@outlook.com>
2025-03-01 22:33:14 +0100

    250301 function rearrange

--- UAM_impedance_components.py
+++ UAM_impedance_components.py
@@ -12,13 +12,13 @@ The metric application is done in the rsp. SQL-scripts.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from scipy.stats import maxwell
+# import pandas as pd
 
 import cm_params
 
 def bathtub2 (x_in, l, r, a1l, a1r):
-    if (x_in > 3.0 * r):   # avoid out-of-range errors
+    if (x_in > 3.0 * r):   # avoid out-of-range errors; ONLY FOR 'U'-SHAPE !!!
         return 1
     elif (x_in < ((l + r) / 2)):
         return (1 / (1 + np.exp( a1l * (x_in - l)) ))
@@ -31,35 +31,32 @@ def PAX_max(x_in, d, w, s):
 	return 1-d*maxwell.pdf(w*(x_in - s))
 
 def TTIME_Logit(x_in, p, a2):
-    if (x_in >= 0 and x_in > 3.0 * p):   # avoid out-of-range errors
-        return 0
+    if (x_in > 3.0 * p):   # avoid out-of-range errors
+        if (a2 >= 0.0):
+            return 0
+        elif (a2 < 0.0):
+            return 1
+        else:
+            raise ValueError("CM: Invalid Thresholds")
     else:
         return (1 / (1 + np.exp(a2 * (x_in - p)) ))
 
 
-## Create values for variables (visualization/plot only, not parametrization)
-max_rat = 3
-max_PAX = 15
-max_dist = 500
-max_thresh = 100
-
-x_rat = np.arange(0.01, max_rat, 0.01).tolist()
-x_PAX = np.arange(0.01, max_PAX, 0.01).tolist()
-x_dist = np.arange(0, max_dist, 0.01).tolist()
-x_thresh = np.arange(0.01, max_thresh, 0.01).tolist()
-
-#### (1) plot distance impedence (bathtub)
+#### (1) plot distance impedence ('bathtub')
 
 ## Set parameters
 shift_left_dist = 75.0
 shift_right_dist = 350.0
-a1l_in=0.1
-a1r_in=0.1
+a1l_in_dist = 0.1
+a1r_in_dist = 0.1
 
-## Create values with function and plot
 bathtub_vals = []
+
+max_dist = 500
+x_dist = np.arange(0.0, max_dist, 0.01).tolist()
+
 for i in x_dist:
-    bathtub_vals.append(bathtub2(i, shift_left_dist, shift_right_dist, a1l_in, a1r_in))
+    bathtub_vals.append(bathtub2(i, shift_left_dist, shift_right_dist, a1l_in_dist, a1r_in_dist))
 
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
@@ -72,14 +69,17 @@ plt.show()
 plt.clf()
 
 
-#### (2) Plot travel time impedance (Logit)
+#### (2) Plot travel time impedance ('Logit')
 
 ## Set parameters
 p_in = 1.0
 a2_in = 5.0
 
-
 TTIME_Logit_vals = []
+
+max_rat = 3
+x_rat = np.arange(0.0, max_rat, 0.01).tolist()
+
 for i in x_rat:
     TTIME_Logit_vals.append(TTIME_Logit(i, p_in, a2_in))
     
@@ -97,7 +97,12 @@ plt.show()
 plt.clf()
 
 
-#### (3.1) plot capacity impedence ('Maxwell')
+#### (3.0) Passengers
+
+max_PAX = 15
+x_PAX = np.arange(0.0, max_PAX, 0.01).tolist()
+
+#### (3.1) plot vehicle capacity impedence ('Maxwell')
 
 ## Set parameters
 d_in = 1.7034
@@ -105,6 +110,7 @@ w_in = 0.35
 s_in = 0.0
 
 PAX_vals = []
+
 for i in x_PAX:
     PAX_vals.append(PAX_max(i, d_in, w_in, s_in))
 
@@ -120,7 +126,7 @@ plt.clf()
 
 
 
-#### (3.2) plot capacity impedence ('bathtub2')
+#### (3.2) plot vehicle capacity impedence ('bathtub2')
 
 ## Set parameters
 shift_left_PAX = 2.0
@@ -129,6 +135,7 @@ a1l_in_pax = 4.0
 a1r_in_pax = 4.0
 
 PAX_vals2 = []
+
 for i in x_PAX:
     PAX_vals2.append(bathtub2(i, shift_left_PAX, shift_right_PAX, a1l_in_pax, a1r_in_pax))
 
@@ -143,30 +150,61 @@ plt.show()
 plt.clf()
 
 
-#### (4) Plot demand threshold (Logit)
+#### (4.0) Total demand threshold
+
+max_thresh = 100
+x_thresh = np.arange(0.0, max_thresh, 0.01).tolist()
+
+#### (4.1) No minimum ('Logit')
 
 ## Set parameters
 p_thresh_in = 50.0
-a2_thresh_in = 5.0 # negative for reverse
+a2_thresh_in = -5.0 # positive/negative for reverse
 
 
-demand_threshold_vals = []
+demand_threshold_logit = []
+
 for i in x_thresh:
-    demand_threshold_vals.append(TTIME_Logit(i, p_thresh_in, a2_thresh_in))
+    demand_threshold_logit.append(TTIME_Logit(i, p_thresh_in, a2_thresh_in))
     
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
-plt.plot(x_thresh, demand_threshold_vals)
+plt.plot(x_thresh, demand_threshold_logit)
 plt.xlabel('Total demand [PAX / d]')
-plt.ylabel('Vertiport Efficiency (normalized)')
-
-# Plot singularity
-# plt.scatter(0, 1, s=100, facecolors='none', edgecolors='#1f77b4')
+plt.ylabel('UAM Impedance (normalized)')
 
 # Plot vertiport throughput
 plt.axvline(p_thresh_in * 0.95, color='red', linestyle='dashed', linewidth=1)
 
-plt.savefig('C:/Users/chris/plots/Imp_TBD_logit.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
-#plt.savefig('C:/Users/chris/plots/Imp_TBD_logit.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
+plt.savefig('C:/Users/chris/plots/Imp_TBD1_logit.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
+#plt.savefig('C:/Users/chris/plots/Imp_TBD1_logit.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
 plt.show()
 plt.clf()
+
+
+#### (4.2) With minimum ('bathtub2')
+
+## Set parameters
+shift_left_demand = 20.0
+shift_right_demand = 50.0
+a1l_in_demand = 4.0
+a1r_in_demand = 4.0
+
+demand_threshold_bathtub = []
+
+for i in x_thresh:
+    demand_threshold_bathtub.append(bathtub2(i, shift_left_demand, shift_right_demand, a1l_in_demand, a1r_in_demand))
+
+plt.figure()
+plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
+plt.plot(x_thresh, demand_threshold_bathtub)
+plt.xlabel('Total demand [PAX / d]')
+plt.ylabel('UAM Impedance (normalized)')
+plt.savefig('C:/Users/chris/plots/Imp_TBD2_bathtub2.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
+#plt.savefig('C:/Users/chris/plots/Imp_TBD2_bathtub2.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
+plt.show()
+plt.clf()
+
+
+
+
