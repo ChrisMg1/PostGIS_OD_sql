1004544c94a80aeeb433fe67dc4586a644e519b4
ChrisMg1 <christoph.maget@outlook.com>
2025-05-16 15:39:35 +0200

    function extension

--- UAM_impedance_components.py
+++ UAM_impedance_components.py
@@ -16,27 +16,19 @@ from scipy.stats import maxwell
 
 import cm_params
 
-def bathtub2 (x_in, l, r, a1l, a1r, transp):
+def bathtub2 (x_in, li, re, a1l, a1r, transp, Nmultip):
+    l = Nmultip * li
+    r = Nmultip * re    
     if (x_in > 2.0 * r):   # avoid out-of-range errors; ONLY FOR 'U'-SHAPE !!! threshold seems sufficient for bathtub, not for logit
         return 1.0
     elif (x_in < ((l + r) * transp)):
+        #return 1.0-(1.0 / (1.0 + (np.exp(-a1l * (x_in - l )) ))) # alternative line
         return (1.0 / (1.0 + (np.exp( a1l * (x_in - l) )) ))
     elif (x_in >= ((l + r) * transp)):
         return (1.0 / (1.0 + (np.exp(-a1r * (x_in - r) )) ))
     else:
         raise ValueError("CM: Invalid Thresholds etc.")
         
-        
-def bathtub3 (x_in, l, r, a1l, a1r, transp):
-    if (x_in > 2.0 * r):   # avoid out-of-range errors; ONLY FOR 'U'-SHAPE !!! threshold seems sufficient for bathtub, not for logit
-        return 1.0
-    elif (x_in < ((l + r) * transp)):
-        return 1.0-(1.0 / (1.0 + 25.0*(np.exp(-a1l * (x_in - l) )) ))
-    elif (x_in >= ((l + r) * transp)):
-        return (1.0 / (1.0 +        (np.exp(-a1r * (x_in - r) )) ))
-    else:
-        raise ValueError("CM: Invalid Thresholds etc.")
-
 def PAX_max(x_in, d, w, s):
 	return 1.0-d*maxwell.pdf(w*(x_in - s))
 
@@ -54,9 +46,7 @@ def TTIME_Logit(x_in, p, a2):
         raise ValueError("CM: Invalid Thresholds etc.")
 
 
-
-
-output_folder_ic = 'C:/Users/chris/plots/v04/factorFunctions/'
+output_folder_ic = 'C:/Users/chris/plots/v06/factorFunctions/'
 
 print('(1) Plot distance impedence (bathtub)')
 
@@ -73,7 +63,7 @@ max_dist = 500
 x_dist = np.arange(0.0, max_dist, 0.01).tolist()
 
 for i in x_dist:
-    bathtub_vals.append(bathtub2(i, shift_left_dist, shift_right_dist, a1l_in_dist, a1r_in_dist, transition_point))
+    bathtub_vals.append(bathtub2(i, shift_left_dist, shift_right_dist, a1l_in_dist, a1r_in_dist, transition_point, 1))
 
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
@@ -86,8 +76,8 @@ plt.axvline(shift_left_dist * 1, color='green', linestyle='dashed', linewidth=1)
 plt.axvline(shift_right_dist * 1, color='red', linestyle='dashed', linewidth=1) # max
 
 #plt.savefig(output_folder_ic + 'Imp_Distance_bathtub2.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
-#plt.savefig(output_folder_ic + 'Imp_Distance_bathtub2.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
-#plt.show()
+plt.savefig(output_folder_ic + 'Imp_Distance_bathtub2.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
+plt.show()
 plt.clf()
 
 print('(2) Plot travel time impedance (Logit)')
@@ -98,8 +88,8 @@ a2_in = 5.0 # default: 5.0
 
 TTIME_Logit_vals = []
 
-max_rat = 3
-x_rat = np.arange(0.0, max_rat, 0.01).tolist()
+
+x_rat = np.arange(0.0, 3.0, 0.01).tolist()
 
 for i in x_rat:
     TTIME_Logit_vals.append(TTIME_Logit(i, p_in, a2_in))
@@ -113,7 +103,7 @@ plt.ylabel('UAM Impedance (normalized)')
 # Plot singularity
 # plt.scatter(0, 1, s=100, facecolors='none', edgecolors='#1f77b4')
 #plt.savefig(output_folder_ic + 'Imp_TTratio_logit.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
-#plt.savefig(output_folder_ic + 'Imp_TTratio_logit.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
+plt.savefig(output_folder_ic + 'Imp_TTratio_logit.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
 plt.show()
 plt.clf()
 
@@ -159,7 +149,7 @@ transition_point = 0.5 # Transition from left-hand (logit) function to right-han
 PAX_vals2 = []
 
 for i in x_PAX:
-    PAX_vals2.append(bathtub2(i, shift_left_PAX, shift_right_PAX, a1l_in_pax, a1r_in_pax, transition_point))
+    PAX_vals2.append(bathtub2(i, shift_left_PAX, shift_right_PAX, a1l_in_pax, a1r_in_pax, transition_point, 1))
 
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
@@ -174,8 +164,8 @@ plt.clf()
 
 #### (4.0) Total demand threshold
 
-max_thresh = 1000
-x_thresh = np.arange(0.0, max_thresh, 0.01).tolist()
+
+x_demand = np.arange(0.0, 2500, 0.5).tolist() # ggf. wieder auf 1000 und 0.1
 
 print('(4.1) No minimum (Logit)')
 
@@ -186,12 +176,12 @@ a2_thresh_in = -0.1 # positive/negative for reverse
 
 demand_threshold_logit = []
 
-for i in x_thresh:
+for i in x_demand:
     demand_threshold_logit.append(TTIME_Logit(i, p_thresh_in, a2_thresh_in))
     
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
-plt.plot(x_thresh, demand_threshold_logit)
+plt.plot(x_demand, demand_threshold_logit)
 plt.xlabel('Demand [PAX / day]')
 plt.ylabel('UAM Impedance (normalized)')
 
@@ -204,10 +194,12 @@ plt.axvline(p_thresh_in * 1, color='red', linestyle='dashed', linewidth=1)
 plt.clf()
 
 
+
+
 print('(4.2) With minimum (bathtub2)')
 
 ## Set parameters
-shift_left_demand = 48.0 # Approx. 1 flight per hour: 24*4=96 _OR_ 12 flights per day: 12*4=48
+shift_left_demand = 96.0 # Approx. 1 flight per hour: 24*4=96 _OR_ 12 flights per day: 12*4=48
 shift_right_demand = 768.0 # 32.0*24.0=768: According paper Lukas, 32 PAX per hour
 a1l_in_demand = 0.1
 a1r_in_demand = 0.1
@@ -215,12 +207,12 @@ transition_point = 0.5 # Transition from left-hand (logit) function to right-han
 
 demand_threshold_bathtub = []
 
-for i in x_thresh:
-    demand_threshold_bathtub.append(bathtub2(i, shift_left_demand, shift_right_demand, a1l_in_demand, a1r_in_demand, transition_point))
+for i in x_demand:
+    demand_threshold_bathtub.append(bathtub2(i, shift_left_demand, shift_right_demand, a1l_in_demand, a1r_in_demand, transition_point, 1))
 
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
-plt.plot(x_thresh, demand_threshold_bathtub)
+plt.plot(x_demand, demand_threshold_bathtub)
 plt.xlabel('Demand [PAX / day]')
 plt.ylabel('UAM Impedance (normalized)')
 
@@ -234,31 +226,35 @@ plt.axvline(shift_right_demand * 1, color='red', linestyle='dashed', linewidth=1
 plt.clf()
 
 
-print('(4.3) With minimum, playing around (bathtub3)')
+print('(4.3) With minimum, playing around (bathtub2/LogBath)')
 
 ## Set parameters
-shift_left_demand = 150.0 # Approx. 1 flight per hour: 24*4=96 _OR_ 12 flights per day: 12*4=48
+
+shift_left_demand = 288.0 # e.g. 24*3*4=288 or 24*4=96 _OR_ 12 flights per day: 12*4=48
 shift_right_demand = 768.0 # 32.0*24.0=768: According paper Lukas, 32 PAX per hour
 a1l_in_demand = 0.02
 a1r_in_demand = 0.1
-transition_point = 0.75 # Transition from left-hand (logit) function to right-hand (logit) function
-
-demand_threshold_bathtub = []
-
-for i in x_thresh:
-    demand_threshold_bathtub.append(bathtub3(i, shift_left_demand, shift_right_demand, a1l_in_demand, a1r_in_demand, transition_point))
+transition_point = 0.65 # Transition from left-hand (logit) function to right-hand (logit) function
 
 plt.figure()
 plt.grid(color='grey', linestyle='dotted', linewidth=0.5)
-plt.plot(x_thresh, demand_threshold_bathtub)
+
+for n_vert in range(1, 4):
+    demand_threshold_LogBath = []
+    for i in x_demand:
+        demand_threshold_LogBath.append(bathtub2(i, shift_left_demand, shift_right_demand, a1l_in_demand, a1r_in_demand, transition_point, n_vert))
+    plt.plot(x_demand, demand_threshold_LogBath, label=r"$n_{vert}$ = " + str(n_vert))
+
+plt.legend()
 plt.xlabel('Demand [PAX / day]')
 plt.ylabel('UAM Impedance (normalized)')
 
-# Plot vertiport throughput
+# Plot min/max, e.g. vertiport throughput
 #plt.axvline(shift_left_demand * 1, color='green', linestyle='dashed', linewidth=1) # min
-plt.axvline(shift_right_demand * 1, color='red', linestyle='dashed', linewidth=1) # max
+#plt.axvline(shift_right_demand * 1, color='red', linestyle='dashed', linewidth=1) # max
 
-#plt.savefig(output_folder_ic + 'Imp_PAXperDAY_bathtub2.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
-#plt.savefig(output_folder_ic + 'Imp_PAXperDAY_bathtub2.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
+#plt.savefig(output_folder_ic + 'Imp_PAXperDAY_LogBath.png', dpi=600, bbox_inches='tight', transparent=True) ## png/dpi for (hi-res) poster-plot
+plt.savefig(output_folder_ic + 'Imp_PAXperDAY_LogBath.pdf', bbox_inches='tight', transparent=True) ## pdf for LaTeX
 plt.show()
 plt.clf()
+
